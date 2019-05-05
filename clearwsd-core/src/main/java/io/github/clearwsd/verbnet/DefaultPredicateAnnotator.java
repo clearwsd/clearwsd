0715189be6e3d84935bc35cb0d24cd8a7a6d1473
James Gung <gungjm@gmail.com>
2019-05-05 18:17:33 -0400

    Update predicate annotator to handle dep parser errors

--- clearwsd-core/src/main/java/io/github/clearwsd/verbnet/DefaultPredicateAnnotator.java
+++ clearwsd-core/src/main/java/io/github/clearwsd/verbnet/DefaultPredicateAnnotator.java
@@ -16,16 +16,17 @@
 
 package io.github.clearwsd.verbnet;
 
-import io.github.clearwsd.type.DepNode;
-import io.github.clearwsd.type.DepTree;
 import io.github.clearwsd.feature.annotator.Annotator;
 import io.github.clearwsd.feature.util.DepUtils;
 import io.github.clearwsd.feature.util.PosUtils;
+import io.github.clearwsd.type.DepNode;
+import io.github.clearwsd.type.DepTree;
 import io.github.clearwsd.utils.LemmaDictionary;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import static io.github.clearwsd.type.FeatureType.Dep;
+import static io.github.clearwsd.type.FeatureType.Lemma;
 import static io.github.clearwsd.type.FeatureType.Pos;
 import static io.github.clearwsd.type.FeatureType.Predicate;
 
@@ -40,12 +41,22 @@ public class DefaultPredicateAnnotator implements Annotator<DepTree> {
 
     private static final long serialVersionUID = -2953300591005876159L;
 
+    private static final String HAVE = "have";
+
     @Getter
     private final LemmaDictionary dictionary;
 
     @Override
     public DepTree annotate(DepTree instance) {
+        int index = -1;
         for (DepNode token : instance) {
+            ++index;
+            // skip "have to x", since some dependency parsers, like NLP4J, to not mark as an auxiliary
+            if (index + 1 < instance.size()
+                    && token.feature(Lemma).equals(HAVE)
+                    && instance.get(index + 1).feature(Lemma).equals("to")) {
+                continue;
+            }
             if (PosUtils.isVerb(token.feature(Pos)) && !DepUtils.isAux(token.feature(Dep))) {
                 token.addFeature(Predicate, dictionary.apply(token));
             }
